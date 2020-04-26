20c31345e67a461bb32b49a507e8d0ad39a6116f
AKM <ashish.akm@gmail.com>
2020-04-27 00:09:02 +0530

    save changes

---
+++ Migrations/20200426154601_SecondMigration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Todo",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Todo",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
+
+namespace DotNetCoreSqlDb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddAzureWebAppDiagnostics();
+                });
+        //    .ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics())
+        //.ConfigureServices(serviceCollection => serviceCollection
+        // .Configure<AzureFileLoggerOptions>(options =>
+        //    {
+        //        options.FileName = "azure-diagnostics-";
+        //        options.FileSizeLimit = 50 * 1024;
+        //        options.RetainedFileCountLimit = 5;
+        //    }).Configure<AzureBlobLoggerOptions>(options =>
+        //    {
+        //        options.BlobName = "log.txt";
+        //    })
+        //)
+    }
+}
